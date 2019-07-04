acfe8b3833bd474008b127febfd2006320a19e86
jessica <jessicamorim.42@gmail.com>
2019-07-04 17:24:27 -0300

    add get user by id and delete user by id

--- controllers/usersControllers.go
+++ controllers/usersControllers.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"go-api-test/models"
 	"net/http"
 )
@@ -23,4 +24,26 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	}
 	response, status := user.Create()
 	models.Respond(w, response, status)
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var user models.User
+	if result := models.GetDB().Where("id = ?", params["id"]).First(&user); result.Error != nil {
+		models.Respond(w, models.Message("User doesn't exist"), http.StatusBadRequest)
+		return
+	}
+	user.Password = ""
+	json.NewEncoder(w).Encode(&user)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var user models.User
+	if result := models.GetDB().Where("id = ?", params["id"]).First(&user); result.Error != nil {
+		models.Respond(w, models.Message("User doesn't exist"), http.StatusBadRequest)
+		return
+	}
+	models.GetDB().Delete(&user)
+	models.Respond(w, models.Message("User deleted successfully"), http.StatusOK)
 }
--- main.go
+++ main.go
@@ -12,6 +12,8 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/users", controllers.GetUsers).Methods("GET")
 	router.HandleFunc("/users", controllers.CreateUser).Methods("POST")
+	router.HandleFunc("/users/{id}", controllers.GetUser).Methods("GET")
+	router.HandleFunc("/users/{id}", controllers.DeleteUser).Methods("DELETE")
 	router.HandleFunc("/auth", controllers.Authenticate).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
